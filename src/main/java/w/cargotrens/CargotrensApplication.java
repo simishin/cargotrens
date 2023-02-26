b35fef36559bc1e031e2c969a166a18351c66dac
“simishin” <ms67i@yandex.ru>
2023-02-26 20:44:29 +0700

    Старт проекта

---
+++ src/main/java/w/cargotrens/CargotrensApplication.java
@@ -0,0 +1,13 @@
+package w.cargotrens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargotrensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CargotrensApplication.class, args);
+    }
+
+}
---
+++ src/test/java/w/cargotrens/CargotrensApplicationTests.java
@@ -0,0 +1,13 @@
+package w.cargotrens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargotrensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
